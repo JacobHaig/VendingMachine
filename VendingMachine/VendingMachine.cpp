39e94894703d959b72d7d11412b2ac325478446a
Jacob Haig <jacobedwardhaig@gmail.com>
2017-04-04 16:01:16 -0400

    stuff

--- VendingMachine/VendingMachine.cpp
+++ VendingMachine/VendingMachine.cpp
@@ -1,15 +1,13 @@
+
+#include "stdafx.h"
 #include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
 #include <vector>
 #include <string>
-#include "stdafx.h"
 
 using namespace std;
 
-struct COORD
-{
-	int x;
-	int y;
-};
 
 class item {
 public:
@@ -33,31 +31,43 @@ public:
 	bool checkLocation(Vending, int);
 };
 
+Vending::Vending() {
+
+}
+
+void Vending::dropItem() {}
+void Vending::displayPrice() {}
+void Vending::dispenceCash() {}
+void Vending::restock() {}
+void Vending::displayError(string e) {}
+void Vending::reset() {}
+bool Vending::checkInput(int) { return true; }
 bool Vending::checkLocation(Vending Lib, int loc) {
 	if (loc <= Lib.slots.size()) {
-
+		return true;
 	}
-};
+	return false;
+}
 
 int main() {
 
 	bool notDone = true;
 
 	while (notDone) {
-		Vending Lib();
+		Vending Lib;
 
-		cout << “Welcome to VenMach Tech INC.” << endl;
-		cout << “Please make a selection” << endl;
+		cout << "Welcome to VenMach Tech INC." << endl;
+		cout << "Please make a selection" << endl;
 		int input;
 		cin >> input;
 
 		// start of selection
-		if (lib.checkLocation(Lib, input)) {
+		if (Lib.checkLocation(Lib, input)) {
 
 		}
 		else {
-			cout << “Invalid Location” << endl;
-
+			cout << "Invalid Location" << endl;
+			cout << "Please try again" << endl;
 		}
 	}
-
+}

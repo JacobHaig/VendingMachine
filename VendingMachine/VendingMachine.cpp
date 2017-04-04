0a7ca96961f21f1c52d0aef857e49dfa2f0cdb3c
Jacob Haig <jacobedwardhaig@gmail.com>
2017-04-04 17:06:55 -0400

    Added Item list vector

--- VendingMachine/VendingMachine.cpp
+++ VendingMachine/VendingMachine.cpp
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <vector>
+#include <conio.h>
 #include <string>
 
 using namespace std;
@@ -11,15 +12,23 @@ using namespace std;
 
 class item {
 public:
+	item(string, double, int);
 	string name;
 	double price;
 	int qty;
 
 };
 
+item::item(string n, double p, int q) {
+	name = n;
+	price = p;
+	qty = q;
+}
+
+
 class Vending {
 public:
-	vector<item> slots;
+	vector<item> slots = {};
 	Vending();
 	void dropItem();
 	void displayPrice();
@@ -31,10 +40,11 @@ public:
 	bool checkLocation(Vending, int);
 };
 
+
+
 Vending::Vending() {
 
 }
-
 void Vending::dropItem() {}
 void Vending::displayPrice() {}
 void Vending::dispenceCash() {}
@@ -42,32 +52,70 @@ void Vending::restock() {}
 void Vending::displayError(string e) {}
 void Vending::reset() {}
 bool Vending::checkInput(int) { return true; }
-bool Vending::checkLocation(Vending Lib, int loc) {
-	if (loc <= Lib.slots.size()) {
+bool Vending::checkLocation(Vending cafe, int loc) {
+	if (loc <= cafe.slots.size()) {
 		return true;
 	}
-	return false;
+	else {
+		//cout << cafe.slots.size() << endl;
+		return false;
+	}
+}
+
+void Display() {
+
 }
 
 int main() {
+	cout << "Welcome to VenMach Tech INC." << endl;
+	cout << "Please make a selection" << endl;
 
 	bool notDone = true;
+	int itemsInVendingMachine = 40;
+	Vending cafe;
+
+	for (int i = 0; i < itemsInVendingMachine - cafe.slots.size(); i++)
+	{
+		item e("Cake", 1.25, 5);
+		cafe.slots.push_back(e);
+	}
 
 	while (notDone) {
-		Vending Lib;
 
-		cout << "Welcome to VenMach Tech INC." << endl;
-		cout << "Please make a selection" << endl;
-		int input;
-		cin >> input;
+		string e;
+		vector<int> inputs;
+		do
+		{
+			int input;
+			cin >> input;
+			if (cafe.checkLocation(cafe, input))
+			{
+				inputs.push_back(input);
+			}
+			else {
+				cout << "Invalid Location" << endl;
+				cout << "Please try again" << endl;
+			}
+			cout << "Would you like to pick more? N/Y" << endl;
+			cin >> e;
+
+		} while (e == "Y" || e == "y");
+
+
 
 		// start of selection
-		if (Lib.checkLocation(Lib, input)) {
+		for each (int _item in inputs)
+		{
+			if (cafe.checkLocation(cafe, _item)) {
+				cout << endl << "The " << cafe.slots[_item].name << "'s price is " << cafe.slots[_item].price << "." << endl << endl;
 
+			}
 		}
-		else {
-			cout << "Invalid Location" << endl;
-			cout << "Please try again" << endl;
-		}
+
+		cout << "Would you like check out?" << endl;
+
 	}
+	cout << "Thank you shop again!" << endl;
+
+	getch();
 }
